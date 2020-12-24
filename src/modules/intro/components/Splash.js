4bce3d63df2f956a8b15a1553c4c3a504c1dc75a
Nishant <nishant@kustard.io>
2020-12-24 12:32:22 +0530

    splash svg added

--- src/modules/intro/components/Splash.js
+++ src/modules/intro/components/Splash.js
@@ -1,36 +1,36 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect} from 'react';
-import {View, Text, Image} from 'react-native';
+import {View} from 'react-native';
 import SafeArea from '../../commons/components/SafeArea';
 import {styles} from '../styles/introStyle';
-import EStyleSheet from 'react-native-extended-stylesheet';
 import {Colors} from '../../../utils/values/Colors';
 import {Actions} from 'react-native-router-flux';
 import {connect} from 'react-redux';
-// import {getStoreInfo} from '../../onboarding/Api';
+import SplashLogo from '../../../assets/images/splash-logo.svg';
 import {getAuthToken} from '../../../utils/utility/LocalStore';
 import {getUserDetails} from '../../home/Api';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import {setToken} from '../../authentication/AuthActions';
 
 const Splash = (props) => {
   useEffect(() => {
-    // setTimeout(()=>Actions.replace('introduction'),500)
-    (async () => {
-      const token = await getAuthToken();
-
-      if (token) {
-        props.getUserDetails();
-      } else {
-        Actions.reset('introduction');
-      }
-    })();
+    handleNavigation();
   }, []);
-
+  const handleNavigation = async () => {
+    const token = await getAuthToken();
+    if (token) {
+      props.getUserDetails();
+      props.setToken(token);
+    } else {
+      Actions.reset('introduction');
+    }
+  };
   return (
     <SafeArea statusBarColor={Colors.themeGreen}>
       <View style={styles.container}>
-        <Image
-          source={require('../../../assets/images/logo.png')}
-          style={styles.splashLogo}
+        <SplashLogo
+          height={EStyleSheet.value('40rem')}
+          width={EStyleSheet.value('210rem')}
         />
       </View>
     </SafeArea>
@@ -41,6 +41,7 @@ const mapStateToProps = (state) => ({});
 
 const mapDispatchToProps = {
   getUserDetails,
+  setToken,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Splash);
