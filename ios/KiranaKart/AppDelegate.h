364ed1cc12261eda996998d84a3f563805361496
Nishant <nishant@kustard.io>
2021-01-21 16:07:12 +0530

    clevertap ios changes

--- ios/KiranaKart/AppDelegate.h
+++ ios/KiranaKart/AppDelegate.h
@@ -1,8 +1,10 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
+@property (nonatomic,strong) NSDictionary *resp;
 
 @end
